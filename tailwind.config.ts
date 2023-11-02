52c66ad79df15fd8c5d257185fd4703223d56e0d
Chidex <gchidex08@gmail.com>
2023-11-02 02:25:30 +0000

    Set up prettier, husky, and some custom theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                green: '#A0FF1F',
+                pink: '#F70087',
+                black: '#000000',
+                white: '#FFFFFF',
+                gray: {
+                    dark: '#5B5B5B',
+                    DEFAULT: '#C3C3C3',
+                    light: '#F8F8F8',
+                },
+                success: '#00ED71',
+                error: '#F40256',
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
