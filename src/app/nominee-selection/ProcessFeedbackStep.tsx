dbc6b6e7269e7dcbbe1008d7714e8f8cb953b03d
Chidex <gchidex08@gmail.com>
2023-11-05 11:52:00 +0000

    Fix bug when selection feedback in mobile view

--- src/app/nominee-selection/ProcessFeedbackStep.tsx
+++ src/app/nominee-selection/ProcessFeedbackStep.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import Image from 'next/image';
 import { UseFormRegister } from 'react-hook-form';
+import {
+    BsFillEmojiSmileFill,
+    BsFillEmojiNeutralFill,
+    BsFillEmojiFrownFill,
+} from 'react-icons/bs';
+import { MdEmojiEmotions } from 'react-icons/md';
+import { HiEmojiSad } from 'react-icons/hi';
 import ProgressBar from '@/components/ProgressBar';
+import useScreenSize from '@/hooks/useScreen';
 import { FormInputs } from './type';
 
-const ThirdFormStep = (props: {
+const ProcessFeedbackStep = (props: {
     register: UseFormRegister<FormInputs>;
     errMsg: string | null;
 }) => {
+    const screenSize = useScreenSize();
+
     return (
         <>
             <div>
@@ -28,68 +40,11 @@ const ThirdFormStep = (props: {
                     pick the cube of the month. What&apos;s your opinion on this
                     method?
                 </p>
-                <label
-                    htmlFor='fairnessRange'
-                    className='block mb-2 font-bold my-4'
-                >
-                    Process opinion
-                </label>
-                <input
-                    type='range'
-                    min='1'
-                    max='100'
-                    value='50'
-                    id='fairnessRange'
-                    className='w-full mb-10 hidden tablet:block'
-                    {...props.register('process')}
-                />
-                <div className='tablet:hidden'>
-                    <div className='w-full flex justify-between border border-gray p-3 mb-4'>
-                        <label htmlFor='very-unfair'>Very unfair</label>
-                        <input
-                            type='radio'
-                            id='very-unfair'
-                            name='fav_language'
-                            value='HTML'
-                        />
-                    </div>
-                    <div className='w-full flex justify-between border border-gray p-3 mb-4'>
-                        <label htmlFor='css'>Unfair</label>
-                        <input
-                            type='radio'
-                            id='unfair'
-                            name='fav_language'
-                            value='CSS'
-                        />
-                    </div>
-                    <div className='w-full flex justify-between border border-gray p-3 mb-4'>
-                        <label htmlFor='javascript'>Not sure</label>
-                        <input
-                            type='radio'
-                            id='not-sure'
-                            name='fav_language'
-                            value='JavaScript'
-                        />
-                    </div>
-                    <div className='w-full flex justify-between border border-gray p-3 mb-4'>
-                        <label htmlFor='javascript'>Fair</label>
-                        <input
-                            type='radio'
-                            id='fair'
-                            name='fav_language'
-                            value='JavaScript'
-                        />
-                    </div>
-                    <div className='w-full flex justify-between border border-gray p-3 mb-6'>
-                        <label htmlFor='javascript'>Very Fair</label>
-                        <input
-                            type='radio'
-                            id='very-fair'
-                            name='fav_language'
-                            value='JavaScript'
-                        />
-                    </div>
-                </div>
+                {screenSize.width >= 849 ? (
+                    <Slider register={props.register} />
+                ) : (
+                    <Radio register={props.register} />
+                )}
                 {props.errMsg ? (
                     <p className='text-error font-anonymous'>{props.errMsg}</p>
                 ) : null}
@@ -98,4 +53,106 @@ const ThirdFormStep = (props: {
     );
 };
 
-export default ThirdFormStep;
+const Slider = (props: { register: UseFormRegister<FormInputs> }) => (
+    <>
+        <input
+            type='range'
+            min='20'
+            max='100'
+            step='20'
+            id='process'
+            className='w-full mb-10 accent-pink hidden tablet:block'
+            list='markers'
+            {...props.register('process')}
+        />
+        <datalist id='markers' className='flex justify-between w-full'>
+            <option className='flex flex-col font-anonymous' value='20'>
+                <div>
+                    <HiEmojiSad className='w-8 h-8 text-black' />
+                    <span>Very Unfair</span>
+                </div>
+            </option>
+            <option className='font-anonymous' value='40' label='Unfair'>
+                <div>
+                    <BsFillEmojiFrownFill className='w-8 h-8 text-black' />
+                    <span>Unfair</span>
+                </div>
+            </option>
+            <option className='font-anonymous' value='60'>
+                <div>
+                    <BsFillEmojiNeutralFill className='w-8 h-8 text-black' />
+                    <span>Not Sure</span>
+                </div>
+            </option>
+            <option className='font-anonymous' value='80'>
+                <div>
+                    <BsFillEmojiSmileFill className='w-8 h-8 text-black' />
+                    <span>Fair</span>
+                </div>
+            </option>
+            <option className='font-anonymous' value='100' label='Very Fair'>
+                <div>
+                    <MdEmojiEmotions className='w-8 h-8 text-black' />
+                    <span>Very Fair</span>
+                </div>
+            </option>
+        </datalist>
+    </>
+);
+
+const Radio = (props: { register: UseFormRegister<FormInputs> }) => (
+    <div className='tablet:hidden'>
+        <div className='w-full flex justify-between border border-gray p-3 mb-4'>
+            <label htmlFor='very-unfair'>Very Unfair</label>
+            <input
+                type='radio'
+                id='very_unfair'
+                className='accent-pink'
+                value='20'
+                {...props.register('process')}
+            />
+        </div>
+        <div className='w-full flex justify-between border border-gray p-3 mb-4'>
+            <label htmlFor='unfair'>Unfair</label>
+            <input
+                type='radio'
+                id='unfair'
+                className='accent-pink'
+                value='40'
+                {...props.register('process')}
+            />
+        </div>
+        <div className='w-full flex justify-between border border-gray p-3 mb-4'>
+            <label htmlFor='not_sure'>Not sure</label>
+            <input
+                type='radio'
+                id='not_sure'
+                className='accent-pink'
+                value='60'
+                {...props.register('process')}
+            />
+        </div>
+        <div className='w-full flex justify-between border border-gray p-3 mb-4'>
+            <label htmlFor='fair'>Fair</label>
+            <input
+                type='radio'
+                id='fair'
+                className='accent-pink'
+                value='80'
+                {...props.register('process')}
+            />
+        </div>
+        <div className='w-full flex justify-between border border-gray p-3 mb-6'>
+            <label htmlFor='very_fair'>Very Fair</label>
+            <input
+                type='radio'
+                id='very_fair'
+                className='accent-pink'
+                value='100'
+                {...props.register('process')}
+            />
+        </div>
+    </div>
+);
+
+export default ProcessFeedbackStep;
